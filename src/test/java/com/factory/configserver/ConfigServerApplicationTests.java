33414c8ef58a34125b6a6a47326ce99bdc5a2a77
Francisco Salas Lazcano <fsalaslazcano@gmail.com>
2021-06-01 21:29:18 -0400

    First Commit for config server

---
+++ src/test/java/com/factory/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factory.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
